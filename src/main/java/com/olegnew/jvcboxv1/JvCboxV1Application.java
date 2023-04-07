167f6d0f450317a4341100dfcf9a3518c95556b8
oleg-new <9213195@gmail.com>
2023-04-07 18:47:16 +0300

    A project has been created that includes: Spring Boot, Spring MVC, Spring Security having starters     spring-boot-starter-data-jpa     spring-boot-starter-security     spring-boot-starter-web created packages "model, dto, service, repository, controller" for application layers.

---
+++ src/main/java/com/olegnew/jvcboxv1/JvCboxV1Application.java
@@ -0,0 +1,13 @@
+package com.olegnew.jvcboxv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvCboxV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvCboxV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/olegnew/jvcboxv1/JvCboxV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olegnew.jvcboxv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvCboxV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
